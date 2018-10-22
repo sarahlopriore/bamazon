64d4e049ca372ae05eae0476dedc506c5e8db255
unknown <sarah.lopriore@gmail.com>
2018-10-22 17:47:05 -0400

    customer working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -23,7 +24,63 @@ var startApp = function() {
 
     connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        console.table(res);
+
+    });
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose the item number you wish to purchase",
+            choices: ["KS11", "KS22", "KS33", "HG11", "HG22", "ET11", "ET22", "CS11", "OS11", "OS22"],
+            name: "itemChoice"
+        }, {
+            type: "input",
+            message: "How many units of this item would you like to purchase?",
+            name: "numberChoice"
+        }
+    ])
+    .then(function(answers) {
+        var item = answers.itemChoice;
+        connection.query("SELECT item_id, stock_quantity, price FROM products WHERE item_id = ?", [item], function(err, res) {
+            if (err) throw err;
+            if (res[0].stock_quantity < answers.numberChoice) {
+                console.log("Insufficient quantity!")
+            } else {
+                console.log(res);
+                var difference = res[0].stock_quantity - answers.numberChoice;
+                var userQ = answers.numberChoice;
+                buyItems(difference, item);
+                showTotal(item, userQ);
+            }
+
+
+        })
+        
     })
+
+    
+}
+
+var buyItems = function(diff, it) {
+    connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: diff
+            },
+            {
+                item_id: it
+            }
+        ])
+
+}
+
+var showTotal = function(it, q) {
+    connection.query("SELECT stock_quantity, price FROM products WHERE item_id = ?", [it], function(err, res) {
+        if (err) throw err;
+        console.log(res[0].price);
+        console.log("Total: " + res[0].price * q);
+    })
+
+    connection.end();
 }
--- seed.sql
+++ seed.sql
@@ -7,15 +7,17 @@ USE bamazon;
 SELECT * FROM products;
 
 CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
+    item_id VARCHAR(4) NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(250) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL,
-    PRIMARY KEY (item_id)
+    PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Cheese Grater", "Kitchen Supplies", 15.50, 8), ("Toaster", "Kitchen Supplies", 29.99, 5), ("Spice Rack", "Kitchen Supplies", 25.00, 4),
-    ("Picture Frame", "Home Goods", 10.99, 12), ("Tapestry", "Home Goods", 35.95, 6), ("Headphones", "Electronics", 85.00, 3), ("HD Television", "Electronics", 399.99, 2),
-    ("Boots", "Clothing/Shoes", 59.99, 7), ("Notecards", "Office Supplies", 3.99, 15), ("Clipboard", "Office Supplies", 12.99, 9);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("KS11", "Cheese Grater", "Kitchen Supplies", 15.50, 8), ("KS22", "Toaster", "Kitchen Supplies", 29.99, 5), ("KS33", "Spice Rack", "Kitchen Supplies", 25.00, 4),
+    ("HG11", "Picture Frame", "Home Goods", 10.99, 12), ("HG22", "Tapestry", "Home Goods", 35.95, 6), ("ET11", "Headphones", "Electronics", 85.00, 3), 
+    ("ET22", "HD Television", "Electronics", 399.99, 2), ("CS11", "Boots", "Clothing/Shoes", 59.99, 7), ("OS11", "Notecards", "Office Supplies", 3.99, 15), 
+    ("OS22", "Clipboard", "Office Supplies", 12.99, 9);
